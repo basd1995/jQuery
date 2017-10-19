699a4296c5d3b693bf4c92152db4d3ed8fd21b83
basd1995 <1035102183@qq.com>
2017-10-19 21:17:05 +0800

    jQuery

---
+++ jQ-tabs-col/JS/main.js
@@ -0,0 +1,12 @@
+$('.tabs>li').on('click', function(){
+	var $this = $(this);
+	var index = $this.index();
+	var $li = $('.panels>li')
+	$this.addClass('active').siblings().removeClass('active');
+	$li.eq(index).addClass('active').siblings().removeClass('active');
+	$('.panels').animate({
+		top: 0-$li.height()*index
+	})
+	console.log(index);
+	console.log($('.panels>li').eq(index));
+})
